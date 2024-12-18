b547de017f517047b8c459e7a88cc8ad5a8c81a0
Ramanan1711 <ajayramanan17@gmail.com>
2024-12-18 10:41:21 +0530

    Initial commit - TrustBank project

---
+++ src/test/java/com/bank/trustbank/TrustBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.trustbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
