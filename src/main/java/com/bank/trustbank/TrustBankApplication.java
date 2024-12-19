b1d1489f8c82ea8dda3f9f22e2b27d1ff70f14a2
Ramanan1711 <ajayramanan17@gmail.com>
2024-12-19 11:30:39 +0530

    Application Started

--- src/main/java/com/bank/trustbank/TrustBankApplication.java
+++ src/main/java/com/bank/trustbank/TrustBankApplication.java
@@ -2,12 +2,18 @@ package com.bank.trustbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrustBankApplication {
+public class TrustBankApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrustBankApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrustBankApplication.class, args);
 	}
-
 }
