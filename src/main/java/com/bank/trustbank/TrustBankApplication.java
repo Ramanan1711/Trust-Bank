2490a0df6111e45e1edcfe56052c5dcf3df1da7d
Ramanan1711 <ajayramanan17@gmail.com>
2024-12-19 11:16:44 +0530

    Revert "application Started Successfully"
    This reverts commit 44d0b39bb1d9d2198a94f9b069d719da11b3ff5d.

--- src/main/java/com/bank/trustbank/TrustBankApplication.java
+++ src/main/java/com/bank/trustbank/TrustBankApplication.java
@@ -2,18 +2,12 @@ package com.bank.trustbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrustBankApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TrustBankApplication.class);
-	}
+public class TrustBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrustBankApplication.class, args);
 	}
+
 }
